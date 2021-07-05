8d3efa125b0f27ebb2f7b3d5925a5910240a0254
Ivan-Bradvica <85823639+Ivan-Bradvica@users.noreply.github.com>
2021-07-05 22:29:20 +0200

    Add files via upload

---
+++ projekt.js
@@ -0,0 +1,34 @@
+let alert1 = document.getElementById("alert1")
+let alert2 = document.getElementById("alert2")
+alert1.style.display = "none"
+alert2.style.display = "none"
+
+
+function pretplata_arrow(){
+    let arrow = document.getElementById("arrow")
+    let email = document.getElementById("email").value
+    
+    if(email.includes("@")){
+        arrow.style.display = "inline"
+    } else{
+            arrow.style.display = "none"
+        }
+
+        }
+
+        
+function pretplata(){
+    let email = document.getElementById("email").value
+
+    if(email.length > 10){
+        alert1.style.display = "inline"
+        setTimeout(function(){
+            alert1.style.display = "none"
+        }, 3000)
+    } else{
+        alert2.style.display = "inline"
+        setTimeout(function(){
+            alert2.style.display = "none"
+        }, 3000)
+    }
+}
